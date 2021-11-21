55a507028e74e8c59da127ee16d0af986e7b6ace
Maciej Ziaja <maciejzjg@gmail.com>
2021-11-21 17:28:43 +0100

    Add missing new tests file

---
+++ data_processing/test_data_ingestion.py
@@ -0,0 +1,73 @@
+import datetime
+import json
+
+from data_ingestion import (
+    NoFluffJobsPostingsDataSource,
+    make_data_key,
+    make_json_string)
+
+
+class MockResponse:
+    @staticmethod
+    def json():
+        return {'postings': 'mock_postings_list',
+                'totalCount': 'mock_total_count'}
+
+
+class TestNoFluffJobsPostingsDataSource:
+    def test_returns_correct_metadata_source_name(self, mocker):
+        mocker.patch('requests.get', return_value=MockResponse())
+        results = NoFluffJobsPostingsDataSource.get()
+        source_name = results.metadata.source_name
+        assert source_name == NoFluffJobsPostingsDataSource.SOURCE_NAME
+
+    def test_returns_correct_metadata_datetime(self, mocker):
+        expected = datetime.datetime(2021, 12, 1, 8, 30, 5)
+        datetime_mock = mocker.patch('datetime.datetime')
+        datetime_mock.now.return_value = expected
+
+        mocker.patch('requests.get', return_value=MockResponse())
+
+        results = NoFluffJobsPostingsDataSource.get()
+        obtained_datetime = results.metadata.obtained_datetime
+        assert obtained_datetime == expected
+
+
+def test_make_data_key_returns_correct_key(mocker):
+    datetime_ = datetime.datetime(2021, 12, 1, 8, 30, 5)
+    expected = '1638343805_nofluffjobs'
+    datetime_mock = mocker.patch('datetime.datetime')
+    datetime_mock.now.return_value = datetime_
+
+    mocker.patch('requests.get', return_value=MockResponse())
+
+    data = NoFluffJobsPostingsDataSource.get()
+    result = make_data_key(data)
+    assert result == expected
+
+
+def test_make_json_string_returns_json_with_correct_structure(mocker):
+    mocker.patch('requests.get', return_value=MockResponse())
+
+    data = NoFluffJobsPostingsDataSource.get()
+    result = make_json_string(data)
+    result_back_to_json_dict = json.loads(result)
+
+    assert 'metadata' in result_back_to_json_dict.keys()
+    assert 'data' in result_back_to_json_dict.keys()
+
+
+def test_make_json_string_returns_correct_metadata(mocker):
+    datetime_ = datetime.datetime(2021, 12, 1, 8, 30, 5)
+    expected_json_metatada_str = {
+        'source_name': 'nofluffjobs',
+        'obtained_datetime': '2021-12-01 08:30:05'}
+    datetime_mock = mocker.patch('datetime.datetime')
+    datetime_mock.now.return_value = datetime_
+
+    mocker.patch('requests.get', return_value=MockResponse())
+
+    data = NoFluffJobsPostingsDataSource.get()
+    result = make_json_string(data)
+    result_back_to_json_dict = json.loads(result)
+    assert result_back_to_json_dict['metadata'] == expected_json_metatada_str
