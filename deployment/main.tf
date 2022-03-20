a9ca74fc11abd20b780447e5993ded28455bcd66
Maciej Ziaja <maciejzjg@gmail.com>
2022-03-20 11:33:31 +0100

    Add missing egress to terraform sec group

--- deployment/main.tf
+++ deployment/main.tf
@@ -50,6 +50,13 @@ resource "aws_security_group" "allow_web" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_iam_policy" "allow_s3_bucket_access" {
