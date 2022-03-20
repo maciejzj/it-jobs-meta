305c31598126d614afec87e9f77f5fdcb8bb8ff1
Maciej Ziaja <maciejzjg@gmail.com>
2022-03-20 14:10:45 +0100

    Fix typo in deploy and streamline terraform and docker

--- deployment/main.tf
+++ deployment/main.tf
@@ -44,6 +44,13 @@ resource "aws_security_group" "allow_web" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
     description = "SSH"
     from_port   = 22
     to_port     = 22
